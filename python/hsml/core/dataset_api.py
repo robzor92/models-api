eb9f1a6c2a77b56c6f0fe680b5af9cdd32c7bfbe
robzor92 <robin.eric.andersson@gmail.com>
2021-06-17 16:56:30 +0200

    fix

--- python/hsml/core/dataset_api.py
+++ python/hsml/core/dataset_api.py
@@ -20,6 +20,7 @@ import shutil
 
 from hsml import client, util
 
+import zipfile
 
 class DatasetApi:
     DEFAULT_FLOW_CHUNK_SIZE = 1048576
@@ -35,7 +36,7 @@ class DatasetApi:
         base_params = self._get_flow_base_params(file_name, num_chunks, size)
 
         chunk_number = 1
-        with open(local_abs_path) as f:
+        with zipfile.ZipFile(local_abs_path) as f:
             while True:
               chunk = f.read(self.DEFAULT_FLOW_CHUNK_SIZE)
               if not chunk:
