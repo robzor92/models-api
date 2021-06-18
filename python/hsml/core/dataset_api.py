d414f2438f35a2b74a09c7ef0634a633f82867c1
robzor92 <robin.eric.andersson@gmail.com>
2021-06-18 10:57:32 +0200

    fix

--- python/hsml/core/dataset_api.py
+++ python/hsml/core/dataset_api.py
@@ -34,7 +34,7 @@ class DatasetApi:
         base_params = self._get_flow_base_params(file_name, num_chunks, size)
 
         chunk_number = 1
-        with open(local_abs_path) as f:
+        with open(local_abs_path, 'rb') as f:
             while True:
               chunk = f.read(self.DEFAULT_FLOW_CHUNK_SIZE)
               if not chunk:
