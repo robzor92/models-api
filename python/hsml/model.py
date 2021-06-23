7c609a9bab6536564856922ab1ec3479f80fc435
robzor92 <robin.eric.andersson@gmail.com>
2021-06-23 17:29:31 +0200

    fix

--- python/hsml/model.py
+++ python/hsml/model.py
@@ -111,7 +111,7 @@ class Model:
             "name": self._name,
             "version": self._version,
             "description": self._description,
-            "input_example", self._input_example
+            "input_example": self._input_example
         }
 
     @property
