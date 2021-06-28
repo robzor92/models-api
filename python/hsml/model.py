5ee7fa7f721cea4e2e7768508ffe85e40e6a72ed
robzor92 <robin.eric.andersson@gmail.com>
2021-06-28 10:07:53 +0200

    fix

--- python/hsml/model.py
+++ python/hsml/model.py
@@ -227,7 +227,7 @@ class Model:
         """input_example of the model."""
         if self._input_example is not None and isinstance(self._input_example, str):
             self._dataset_api.download(self._input_example)
-
+            self._input_example = json.loads('inputs.json')
         return self._input_example
 
     @input_example.setter
