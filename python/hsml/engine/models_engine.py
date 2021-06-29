e092300d22c16672c741955a38ae1ac04751326e
robzor92 <robin.eric.andersson@gmail.com>
2021-06-29 14:25:20 +0200

    fix

--- python/hsml/engine/models_engine.py
+++ python/hsml/engine/models_engine.py
@@ -76,13 +76,11 @@ class Engine:
 
         if model_instance.input_example is not None:
             input_example_path = os.getcwd() + "/input_example.json"
-            if self._is_tensor(model_instance.input_example):
-                input_example = self._handle_tensor_input(model_instance.input_example)
-            else:
-                input_example = self._handle_dataframe_input(model_instance.input_example)
+            util.input_example_to_json(model_instance.input_example)
 
             with open(input_example_path, 'w+') as out:
                 json.dump(input_example, out, cls=util.NumpyEncoder)
+
             self._dataset_api.upload(input_example_path, dataset_model_version_path)
             os.remove(input_example_path)
             model_instance.input_example = dataset_model_version_path + "/input_example.json"
@@ -133,78 +131,6 @@ class Engine:
                     except ModelNotFound:
                         pass
                 print("Model not available during polling, set a higher value for await_registration to wait longer.")
-
-    def _is_scalar(self, x):
-        return np.isscalar(x) or x is None
-
-    def _is_tensor(self, x):
-        return isinstance(x, np.ndarray) or (
-            isinstance(x, dict) and all([isinstance(ary, np.ndarray) for ary in x.values()])
-        )
-
-    def _handle_tensor_input(self, input_tensor: Union[np.ndarray, dict]):
-        if isinstance(input_tensor, dict):
-            result = {}
-            for name in input_tensor.keys():
-                result[name] = input_tensor[name].tolist()
-            return {"inputs": result}
-        else:
-            return {"inputs": input_tensor.tolist()}
-
-
-    def _handle_dataframe_input(self, input_ex):
-        if isinstance(input_ex, dict):
-            if all([self._is_scalar(x) for x in input_ex.values()]):
-                input_ex = pd.DataFrame([input_ex])
-            else:
-                raise TypeError(
-                    "Data in the dictionary must be scalar or of type numpy.ndarray"
-                )
-        elif isinstance(input_ex, list):
-            for i, x in enumerate(input_ex):
-                if isinstance(x, np.ndarray) and len(x.shape) > 1:
-                    raise TensorsNotSupportedException(
-                        "Row '{0}' has shape {1}".format(i, x.shape)
-                    )
-            if all([self._is_scalar(x) for x in input_ex]):
-                input_ex = pd.DataFrame([input_ex], columns=range(len(input_ex)))
-            else:
-                input_ex = pd.DataFrame(input_ex)
-        elif not isinstance(input_ex, pd.DataFrame):
-            try:
-                import pyspark.sql.dataframe
-
-                if isinstance(input_example, pyspark.sql.dataframe.DataFrame):
-                    raise Exception(
-                        "Examples can not be provided as Spark Dataframe. "
-                        "Please make sure your example is of a small size and "
-                        "turn it into a pandas DataFrame."
-                    )
-            except ImportError:
-                pass
-            raise TypeError(
-                "Unexpected type of input_example. Expected one of "
-                "(pandas.DataFrame, numpy.ndarray, dict, list), "
-                "got {}".format(type(input_example))
-            )
-        result = input_ex.to_dict(orient="split")
-        # Do not include row index
-        del result["index"]
-        if all(input_ex.columns == range(len(input_ex.columns))):
-            # No need to write default column index out
-            del result["columns"]
-        return result
-
-
-    def read_input_example(self, model_instance, input_example_path):
-        try:
-            tmp_dir = tempfile.TemporaryDirectory(dir=os.getcwd())
-            self._dataset_api.download(input_example_path, tmp_dir.name + '/inputs.json')
-            with open(tmp_dir.name + '/inputs.json', 'rb') as f:
-                return json.loads(f.read())
-        finally:
-            if tmp_dir is not None and os.path.exists(tmp_dir.name):
-                tmp_dir.cleanup()
 
 
 
