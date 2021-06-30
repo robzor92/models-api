48a25f893410c94da22e261be9829997dbef3ef5
robzor92 <robin.eric.andersson@gmail.com>
2021-06-30 13:59:40 +0200

    fix

--- python/hsml/engine/models_engine.py
+++ python/hsml/engine/models_engine.py
@@ -131,9 +131,11 @@ class Engine:
     def download(self, model_instance):
         dataset_model_version_path = "Models/" + model_instance._name + "/" + str(model_instance._version)
         self._dataset_api.zip(dataset_model_version_path, block=True, timeout=480)
-        self._dataset_api.download(dataset_model_version_path + ".zip", os.getcwd())
-        util.unzip(model_instance.version + ".zip")
-        os.remove(model_instance.version + ".zip")
+        zip_path = os.getcwd() + "/" + model_instance._name + "_" + str(model_instance._version) + ".zip"
+        self._dataset_api.download(dataset_model_version_path + ".zip", zip_path)
+        ret = util.unzip(zip_path)
+        os.remove(zip_path)
+        return ret
 
     def read_input_example(model_instance, input_example_path):
         try:
