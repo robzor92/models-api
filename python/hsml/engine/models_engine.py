99eefd2ebcc385322cdb2c2840729312d35b92ad
robzor92 <robin.eric.andersson@gmail.com>
2021-08-03 14:41:53 +0200

    fix

--- python/hsml/engine/models_engine.py
+++ python/hsml/engine/models_engine.py
@@ -88,7 +88,7 @@ class Engine:
 
         if model_instance.signature is not None:
             signature_path = os.getcwd() + "/signature.json"
-            signature = json.load(model_instance.signature.json())
+            signature = model_instance.signature
 
             with open(signature_path, 'w+') as out:
                 print("siggy")
