0689008f22157c97430f11dd515653cf75a755e0
robzor92 <robin.eric.andersson@gmail.com>
2021-08-13 11:05:12 +0200

    fix

---
+++ python/hsml/core/provenance_api.py
@@ -0,0 +1,43 @@
+#
+#   Copyright 2021 Logical Clocks AB
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#
+
+from hsml import client, util
+
+class ProvenanceApi:
+
+    def get_model_td(self, model_id):
+        """Save model metadata to the model registry.
+https://localhost:8181/hopsworks-api/api/project/120/provenance/links?only_apps=true&full_link=true&filter_by=OUT_TYPE:MODEL&filter_by=IN_TYPE:TRAINING_DATASET&filter_by=OUT_ARTIFACT:mnist_1
+        :param remote_path: metadata object of feature group to be saved
+        :type model_instance: Model
+        :return: updated metadata object of the model
+        :rtype: Model
+        """
+        _client = client.get_instance()
+        path_params = [
+            "project",
+            _client._project_id,
+            "provenance",
+            "links"
+        ]
+        query_params = {'only_apps': 'true', 'full_link': 'true', 'filter_by': ['OUT_TYPE:MODEL','IN_TYPE:TRAINING_DATASET','OUT_ARTIFACT:{}'.format(model_id)]}
+        headers = {"content-type": "application/json"}
+        return _client._send_request(
+            "GET",
+            path_params,
+            headers=headers,
+            query_params=query_params
+        )
