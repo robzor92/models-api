5ad071e6042ce7160db34fadeb65ffd6735502c4
robzor92 <robin.eric.andersson@gmail.com>
2021-08-18 11:10:14 +0200

    fix

--- python/hsml/utils/columnar_signature.py
+++ python/hsml/utils/columnar_signature.py
@@ -17,42 +17,45 @@
 from hsml.utils.column import Column
 from typing import Dict, List, Union, Optional
 import pandas
-try:
-    import pyspark
-except:
-    pass
 
 class ColumnarSignature:
     """Metadata object representing a columnar signature for a model."""
 
     def __init__(
             self,
-            columnar_obj: Optional[Union[pandas.core.frame.DataFrame, pyspark.sql.dataframe.DataFrame]] = None
+            columnar_obj: Optional[Union[pandas.core.frame.DataFrame]] = None
     ):
-         if isinstance(columnar_obj, pyspark.sql.dataframe.DataFrame):
-             self.columns = self._convert_spark_to_signature(columnar_obj)
-         else:
-             self.columns = self._convert_pandas_to_signature(columnar_obj)
+        try:
+            import pyspark
+            if isinstance(columnar_obj, pyspark.sql.dataframe.DataFrame):
+                self.columns = self._convert_spark_to_signature(columnar_obj)
+        except:
+            pass
 
-    def _convert_pandas_to_signature(self, columnar_obj):
-        columns = []
         if isinstance(columnar_obj, pandas.DataFrame):
-            pandas_columns = columnar_obj.columns
-            pandas_data_types = columnar_obj.dtypes
-            columns = []
-            for name in pandas_columns:
-                columns.append(Column(name=name, data_type=str(pandas_data_types[name])))
-        elif isinstance(columnar_obj, pandas.Series):
-            columns.append(Column(name='series', data_type=str(columnar_obj.dtypes)))
+            self.columns = self._convert_pandas_df_to_signature(columnar_obj)
+        else:
+            self.columns = self._convert_pandas_series_to_signature(columnar_obj)
+
+    def _convert_pandas_df_to_signature(self, columnar_obj):
+        pandas_columns = columnar_obj.columns
+        pandas_data_types = columnar_obj.dtypes
+        columns = []
+        for name in pandas_columns:
+            columns.append(Column(name=name, data_type=str(pandas_data_types[name])))
+        return columns
+
+    def _convert_pandas_series_to_signature(self, columnar_obj):
+        columns = []
+        columns.append(Column(name='series', data_type=str(columnar_obj.dtypes)))
         return columns
 
     def _convert_spark_to_signature(self, spark_df):
         columns = []
-        if isinstance(spark_df, pyspark.sql.dataframe.DataFrame):
-            types = spark_df.dtypes
-            for dtype in types:
-                name, dtype = dtype
-                columns.append(Column(name=name, data_type=str(dtype)))
+        types = spark_df.dtypes
+        for dtype in types:
+            name, dtype = dtype
+            columns.append(Column(name=name, data_type=str(dtype)))
         return columns
 
     def to_dict(self):
