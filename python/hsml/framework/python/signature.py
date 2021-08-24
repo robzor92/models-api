bacf216289a49fa7c76764eb215dfb90e138f903
robzor92 <robin.eric.andersson@gmail.com>
2021-08-24 16:49:39 +0200

    fix

--- python/hsml/framework/python/signature.py
+++ python/hsml/framework/python/signature.py
@@ -18,7 +18,7 @@ from typing import Optional, Union, TypeVar
 import pandas
 import numpy
 from hsml.utils.signature import Signature
-from hsml.tensorflow.model import Model
+from hsml.framework.python.model import Model
 
 
 def create_model(
